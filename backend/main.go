0e6f7c24a1c7818ead781d8a504b8a991586ed6f
Gaurav Tak <gauravtak996@gmail.com>
2024-10-13 08:53:19 +0530

    initial commit

---
+++ backend/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"backend/routes"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main(){
+
+	err := godotenv.Load(".env")
+
+
+	if err != nil {
+		log.Fatalf("error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	http.HandleFunc("/", routes.GetRoot)
+	Port := fmt.Sprintf(":%s", port)
+	log.Fatal(http.ListenAndServe(Port, nil))
+}
---
+++ backend/routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func GetRoot(w http.ResponseWriter, r *http.Request){
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "You have hit the / endpoint\n")
+}
+
